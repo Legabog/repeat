4445f7adc00a42b6a55ae3d660220c355cde0f31
Oleg <probringer@mail.ru>
2021-04-06 20:34:05 +0300

    added SelectionSort

---
+++ JavaScript/SelectionSort.js
@@ -0,0 +1,23 @@
+// Selection Sort
+
+function SelectionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[minIndex] > array[j]) {
+        minIndex = j
+      }
+    }
+
+    if (minIndex != i) {
+      [array[i], array[minIndex]] = [array[minIndex], array[i]]
+    }
+    
+    
+  }
+
+  return array
+}
+
+console.log(SelectionSort([1, 6, 15, 5, 0]))
