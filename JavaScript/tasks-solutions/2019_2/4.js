bda19c336c44e422a075fcbfe276e6b7d3f37a58
Oleg <probringer@mail.ru>
2021-04-14 18:59:13 +0300

    tasks

--- JavaScript/tasks-solutions/2019_2/4.js
+++ JavaScript/tasks-solutions/2019_2/4.js
@@ -1,5 +1,106 @@
-const readline = require("readline")
+const readline = require("readline");
 
 const rl = readline.createInterface({
-  input: pr
-})
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let string = [];
+
+rl.on("line", (str) => {
+  if (string.length === 0) {
+    if (Number(str.trim()) >= 1 && Number(str.trim()) <= 10 ** 9) {
+      string.push(str.trim());
+    } else {
+      throw new Error("N должно быть в диапазоне [1..10^9].");
+    }
+  } else if (string.length === 1) {
+    if (Number(str.trim()) >= 0 && Number(str.trim()) <= 1000) {
+      string.push(str.trim());
+    } else {
+      throw new Error("M должно быть в диапазоне [0..1000].");
+    }
+  } else if (string.length >= 2) {
+    let tempArray = str.replace(/ +/g, ' ').trim().split("")
+    
+
+    for (let i = 0; i < tempArray.length; i++) {
+      if (tempArray[i] === " ") {
+        if (
+          Number(tempArray.slice(0, i).join("")) >= 1 &&
+          Number(tempArray.slice(0, i).join("")) <= string[0]
+        ) {
+          string.push(tempArray.slice(0, i).join(""));
+          if ((string.length - 2) / 2 > string[i]) {
+            throw new Error("Количество пар чисел должно быть равно M.");
+          }
+        } else {
+          throw new Error(
+            "Числовое значение пары должно быть в диапазоне [1..N]."
+          );
+        }
+
+        if (
+          Number(tempArray.slice(i + 1).join("")) >= 1 &&
+          Number(tempArray.slice(i + 1).join("")) <= string[0]
+        ) {
+          string.push(tempArray.slice(i + 1).join(""));
+          if ((string.length - 2) / 2 > string[i]) {
+            throw new Error("Количество пар чисел должно быть равно M.");
+          }
+        } else {
+          throw new Error(
+            "Числовое значение пары должно быть в диапазоне [1..N]."
+          );
+        }
+      }
+    }
+  } else {
+    throw new Error("Некоректный ввод данных.");
+  }
+});
+
+rl.on("close", () => {
+  let resultIndex = 0;
+  for (let i = 0; i < string.slice(2).length; i += 2) {
+    let temp1 = [];
+    let temp2 = [];
+    let similarValues = 0;
+
+    for (
+      let x = Number(string.slice(2)[i]);
+      x <= Number(string.slice(2)[i + 1]);
+      x++
+    ) {
+      temp1.push(x);
+    }
+
+    for (let j = i + 2; j < string.slice(2).length; j += 2) {
+      for (
+        let x = Number(string.slice(2)[j]);
+        x <= Number(string.slice(2)[j + 1]);
+        x++
+      ) {
+        temp2.push(x);
+      }
+
+      for (let y = 0; y < temp2.length; y++) {
+        if (temp1.indexOf(temp2[y]) !== -1) {
+          similarValues++;
+        }
+      }
+
+      console.log(similarValues)
+      console.log(temp1, temp2);
+
+      if (similarValues === 0) {
+        resultIndex++;
+      }
+      
+      temp2 = [];
+      similarValues = 0
+    }
+  }
+
+  console.log(resultIndex);
+});
--- JavaScript/tasks-solutions/2019_2/6.js
+++ JavaScript/tasks-solutions/2019_2/6.js
@@ -5,10 +5,131 @@ rl = readline.createInterface({
   output: process.stdout
 })
 
-rl.on("line", () => {
+const inputString = []
 
+rl.on("line", (str) => {
+  if (inputString.length === 0) {
+    if (str.trim() >= 3 && str.trim() <= 100) {
+      inputString.push(str.trim())
+    } else {
+      throw new Error("Число вершин многоугольника должно быть в диапазоне [3..100].")
+    }
+  } else if (inputString.length <= Number(inputString[0]) * 2 + 2) {
+    const tempStr = str.replace(/ +/g, ' ').trim().split("")
+
+    console.log(tempStr)
+
+    if (tempStr.indexOf(" ") !== -1) {
+      inputString.push(tempStr.slice(0, tempStr.indexOf(" ")).join())
+      inputString.push(tempStr.slice(tempStr.indexOf(" ") + 1).join())
+    } else {
+      throw new Error("Неверный формат точек.")
+    }
+    console.log(inputString)
+  } else {
+    throw new Error(`Неверное колчичество пар с числовыми координатами. Должно быть ${inputString[0]} и точка.`)
+  }
+
+  console.log(inputString)
 })
 
 rl.on("close", () => {
-  
+  function robustPointInPolygon(vs, point) {
+    var x = point[0]
+    var y = point[1]
+    var n = vs.length
+    var inside = 1
+    var lim = n
+    for(var i = 0, j = n-1; i<lim; j=i++) {
+      var a = vs[i]
+      var b = vs[j]
+      var yi = a[1]
+      var yj = b[1]
+      if(yj < yi) {
+        if(yj < y && y < yi) {
+          var s = orient(a, b, point)
+          if(s === 0) {
+            return 0
+          } else {
+            inside ^= (0 < s)|0
+          }
+        } else if(y === yi) {
+          var c = vs[(i+1)%n]
+          var yk = c[1]
+          if(yi < yk) {
+            var s = orient(a, b, point)
+            if(s === 0) {
+              return 0
+            } else {
+              inside ^= (0 < s)|0
+            }
+          }
+        }
+      } else if(yi < yj) {
+        if(yi < y && y < yj) {
+          var s = orient(a, b, point)
+          if(s === 0) {
+            return 0
+          } else {
+            inside ^= (s < 0)|0
+          }
+        } else if(y === yi) {
+          var c = vs[(i+1)%n]
+          var yk = c[1]
+          if(yk < yi) {
+            var s = orient(a, b, point)
+            if(s === 0) {
+              return 0
+            } else {
+              inside ^= (s < 0)|0
+            }
+          }
+        }
+      } else if(y === yi) {
+        var x0 = Math.min(a[0], b[0])
+        var x1 = Math.max(a[0], b[0])
+        if(i === 0) {
+          while(j>0) {
+            var k = (j+n-1)%n
+            var p = vs[k]
+            if(p[1] !== y) {
+              break
+            }
+            var px = p[0]
+            x0 = Math.min(x0, px)
+            x1 = Math.max(x1, px)
+            j = k
+          }
+          if(j === 0) {
+            if(x0 <= x && x <= x1) {
+              return 0
+            }
+            return 1 
+          }
+          lim = j+1
+        }
+        var y0 = vs[(j+n-1)%n][1]
+        while(i+1<lim) {
+          var p = vs[i+1]
+          if(p[1] !== y) {
+            break
+          }
+          var px = p[0]
+          x0 = Math.min(x0, px)
+          x1 = Math.max(x1, px)
+          i += 1
+        }
+        if(x0 <= x && x <= x1) {
+          return 0
+        }
+        var y1 = vs[(i+1)%n][1]
+        if(x < x0 && (y0 < y !== y1 < y)) {
+          inside ^= 1
+        }
+      }
+    }
+    return 2 * inside - 1
+  }
+
+
 })
